07d7498d245bde275d955e07e7cafb5e3d7fe971
artur <larturl2004@yandex.ru>
2026-03-12 20:14:21 +0300

    Token to local storage

--- frontend/src/api/ApiClient.ts
+++ frontend/src/api/ApiClient.ts
@@ -7,10 +7,17 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const newConfig = { ...config };
+
     newConfig.url = `api/${config.url}`;
 
-    if (newConfig.headers['Content-Type'] === 'multipart/form-data')
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+        newConfig.headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (newConfig.headers['Content-Type'] === 'multipart/form-data') {
         return newConfig;
+    }
 
     if (config.params) {
         newConfig.params = decamelizeKeys(config.params);
@@ -30,7 +37,6 @@ apiClient.interceptors.response.use((response: AxiosResponse) => {
     ) {
         response.data = camelizeKeys(response.data);
     }
-
     return response;
 });
 
