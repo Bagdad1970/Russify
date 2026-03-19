d33065f56fee06f84bacb7aadaf1ba316dcc0fa1
LittleBlBb <apdragon2014@gmail.com>
2026-03-20 01:35:46 +0300

    deleted unused parameter in test

--- RussifyService/src/test/java/ru/russify/russifyservice/controller/TrackControllerTest.java
+++ RussifyService/src/test/java/ru/russify/russifyservice/controller/TrackControllerTest.java
@@ -35,7 +35,6 @@ public class TrackControllerTest {
                 .name("name")
                 .genreId(1L)
                 .authorIds(Set.of(1L))
-//                .albumIds(Set.of(1L))
                 .build();
 
         TrackResponse saved = TrackResponse.builder()
