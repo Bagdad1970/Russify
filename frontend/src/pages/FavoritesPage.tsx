ddc7ec6af4374d0e620113333177f27410399efb
artur <larturl2004@yandex.ru>
2026-03-20 22:50:22 +0300

    Change play next button in favorite tracks

--- frontend/src/pages/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage.tsx
@@ -323,9 +323,9 @@ const FavoritesPage = () => {
                                             </svg>
                                         </button>
                                         <button className="favorites-action-btn" title="Играть следующим">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="none" stroke="#aaa" strokeWidth="2">
-                                                <path d="M8 5v14l11-7z" />
-                                                <path d="M18 5v14" />
+                                            <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" strokeWidth="2">
+                                                <path d="M2 6h20M2 12h12M2 18h8" />
+                                                <path d="M18 15l3 3-3 3M21 18h-6" />
                                             </svg>
                                         </button>
                                     </div>
