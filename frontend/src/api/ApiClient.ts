81c67b8b79fef57b7eec71c394c8ae0853d1dd26
VasiliyOs <vasiliy2809_ne@mail.ru>
2026-03-21 02:20:07 +0300

    fix: fixing AdminDashboard endpoint.

--- frontend/src/api/ApiClient.ts
+++ frontend/src/api/ApiClient.ts
@@ -2,7 +2,7 @@ import axios, {type AxiosResponse, type InternalAxiosRequestConfig} from 'axios'
 import {camelizeKeys, decamelizeKeys} from 'humps';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL_PROD || import.meta.env.VITE_BASE_URL_DEV
+    baseURL: import.meta.env.VITE_BASE_URL_PROD || import.meta.env.VITE_BASE_URL_DEV || 'http://localhost:8080'
 });
 
 apiClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
